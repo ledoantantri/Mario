d1d1312f7729ee44b2d72c281ef639e1ca634f74
ledoantantri <ledoantantri@gmail.com>
2023-06-17 00:44:38 +0700

    Update Stage.cpp

--- Stage.cpp
+++ Stage.cpp
@@ -7,7 +7,7 @@
 
 void CStage::RenderBoundingBox()
 {
-	D3DXVECTOR3 p(x, y, 0);
+	/*D3DXVECTOR3 p(x, y, 0);
 	RECT rect;
 
 	LPTEXTURE bbox = CTextures::GetInstance()->Get(ID_TEX_BBOX);
@@ -25,12 +25,46 @@ void CStage::RenderBoundingBox()
 
 	float yy = y - this->cellHeight / 2 + rect.bottom / 2;
 
-	CGame::GetInstance()->Draw(x - cx, yy - cy, bbox, nullptr, BBOX_ALPHA, rect.right - 1, rect.bottom - 1);
+	CGame::GetInstance()->Draw(x - cx, yy - cy, bbox, nullptr, BBOX_ALPHA, rect.right - 1, rect.bottom - 1);*/
+
+	D3DXVECTOR3 p(x, y, 0);
+	RECT rect;
+
+	LPTEXTURE bbox = CTextures::GetInstance()->Get(ID_TEX_BBOX);
+
+	float l, t, r, b;
+
+	GetBoundingBox(l, t, r, b);
+	rect.left = 0;
+	rect.top = 0;
+	rect.right = (int)r - (int)l;
+	rect.bottom = (int)b - (int)t;
+
+	float cx, cy;
+	CGame::GetInstance()->GetCamPos(cx, cy);
+
+	float xx = x - this->cellWidth / 2 + rect.right / 2;
+
+	CGame::GetInstance()->Draw(xx - cx, y - cy, bbox, nullptr, BBOX_ALPHA, rect.right - 1, rect.bottom - 1);
 }
 
 void CStage::Render()
 {
 	if (this->length <= 0) return;
+	float xx = x;
+	CSprites* s = CSprites::GetInstance();
+
+	s->Get(this->spriteIdBegin)->Draw(xx, y);
+	xx += this->cellWidth;
+	for (int i = 1; i < this->length - 1; i++)
+	{
+		s->Get(this->spriteIdMiddle)->Draw(xx, y);
+		xx += this->cellWidth;
+	}
+	if (length > 1)
+		s->Get(this->spriteIdEnd)->Draw(xx, y);
+
+	/*if (this->length <= 0) return;
 	float yy = y;
 	CSprites* s = CSprites::GetInstance();
 
@@ -42,7 +76,7 @@ void CStage::Render()
 		yy += this->cellHeight;
 	}
 	if (length > 1)
-		s->Get(this->spriteIdEnd)->Draw(x, yy);
+		s->Get(this->spriteIdEnd)->Draw(x, yy);*/
 
 	RenderBoundingBox();
 }
@@ -54,4 +88,10 @@ void CStage::GetBoundingBox(float& l, float& t, float& r, float& b)
 	t = y - this->cellHeight / 2;
 	r = l + this->cellWidth * this->length;
 	b = t + this->cellHeight;
+
+	/*float cellHeight_div_2 = this->cellHeight / 2;
+	l = x - this->cellWidth / 2;
+	t = y - cellHeight_div_2;
+	r = l + this->cellWidth;
+	b = t + this->cellHeight * this->length;*/
 }
